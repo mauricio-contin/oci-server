e11575a207542ab0aeecc1b9b135878a8df499a5
mauricio-contin <128005385+mauricio-contin@users.noreply.github.com>
2024-11-19 09:36:37 -0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -10,10 +10,13 @@ sudo apt update && sudo apt upgrade -y
 
 echo "Instalando dependências..."
 sudo apt install python3 python3-pip curl -y
-pip3 install discord.py oci
+pip3 install discord.py oci schedule
 
 echo "Baixando e instalando OCI CLI..."
 curl -L https://raw.githubusercontent.com/oracle/oci-cli/master/scripts/install/install.sh -o install.sh
 bash install.sh --accept-all-defaults
 
 echo "Configuração concluída. Execute 'oci setup config' para configurar o OCI CLI."
+source ~/.bashrc
+oci --version
+oci setup config
